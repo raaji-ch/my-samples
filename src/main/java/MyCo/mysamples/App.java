0fd6bb73a726ea1bb13381adffce3da14c8ec36b
Eswar <eswar@eswarsmbpro.attlocal.net>
2016-10-11 20:01:55 -0500

    First Commit

---
+++ src/main/java/MyCo/mysamples/App.java
@@ -0,0 +1,15 @@
+package MyCo.mysamples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main( String[] args )
+    {
+        HelloWorld h = new HelloWorld();
+        System.out.println(h.sayHello(args[0]));
+        
+    }
+}
---
+++ src/main/java/MyCo/mysamples/HelloWorld.java
@@ -0,0 +1,13 @@
+package MyCo.mysamples;
+
+public class HelloWorld
+{
+	public HelloWorld(){
+		
+	}
+	
+    public String sayHello(String name)
+    {
+        return "Hello "+name;
+    }
+}
